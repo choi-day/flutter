e753706f0bf068ddfc671b20e03bfe1855dd7c16
choi-day <choisschoco@gmail.com>
2024-07-21 14:39:39 +0900

    flutter first test

---
+++ contact/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp()); //앱 구동시켜주세요
+}
+ //아래 5줄은 기줄
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('앱제목', style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.blue,
+          centerTitle: false,
+        ),
+        body: Container(),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page),
+            ],
+          )
+        ),
+      )
+    );
+  }
+}
